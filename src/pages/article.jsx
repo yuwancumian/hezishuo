3e8aabf4f76384600a6bcccd92a1b9832df53116
yuwacumian <yuwancumian@hotmail.com>
2020-03-13 17:42:50 +0800

    adjust style of pages

--- src/pages/article.jsx
+++ src/pages/article.jsx
@@ -1,18 +1,18 @@
 export default () =>{
   return (
     <div className={'container article'}>
-      <div className="box-2">1</div>
-      <div className="box-2">2</div>
-      <div className="box-2">3</div>
-      <div className="box-2">4</div>
-      <div className="box-2">1</div>
-      <div className="box-2">2</div>
-      <div className="box-2">3</div>
-      <div className="box-2">4</div>
-      <div className="box-2">1</div>
-      <div className="box-2">2</div>
-      <div className="box-2">3</div>
-      <div className="box-2">4</div>
+      <div className="box box-2">1</div>
+      <div className="box box-2">2</div>
+      <div className="box box-2">3</div>
+      <div className="box box-2">4</div>
+      <div className="box box-2">1</div>
+      <div className="box box-2">2</div>
+      <div className="box box-2">3</div>
+      <div className="box box-2">4</div>
+      <div className="box box-2">1</div>
+      <div className="box box-2">2</div>
+      <div className="box box-2">3</div>
+      <div className="box box-2">4</div>
     </div>
   )
 }
--- src/pages/box.jsx
+++ src/pages/box.jsx
@@ -1,18 +1,24 @@
 export default () =>{
   return (
-    <div className={'container box'}>
-      <div className="box-4">1</div>
-      <div className="box-4">2</div>
-      <div className="box-4">3</div>
-      <div className="box-4">4</div>
-      <div className="box-4">5</div>
-      <div className="box-4">6</div>
-      <div className="box-4">7</div>
-      <div className="box-4">8</div>
-      <div className="box-4">9</div>
-      <div className="box-4"></div>
-      <div className="box-4"></div>
-      <div className="box-4"></div>
+    <div className={'container product'}>
+      <div className="box box-3">11</div>
+      <div className="box box-3">22</div>
+      <div className="box box-3">3</div>
+      <div className="box box-3">4</div>
+      <div className="box box-3">5</div>
+      <div className="box box-3">6</div>
+      <div className="box box-3">7</div>
+      <div className="box box-3">8</div>
+      <div className="box box-3">9</div>
+      <div className="box box-3"></div>
+      <div className="box box-3"></div>
+      <div className="box box-3"></div>
+      <div className="box box-3"></div>
+      <div className="box box-3"></div>
+      <div className="box box-3"></div>
+      <div className="box box-3"></div>
+      <div className="box box-3"></div>
+      <div className="box box-3"></div>
     </div>
   )
 }
